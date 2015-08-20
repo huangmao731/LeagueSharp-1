1f22b45f4673ce129836c52eb503ad48778dc9d7
ShineSharp <sunshinebuy@hotmail.com>
2015-08-20 18:15:46 +0300

    .

--- MoonDiana/ShineCommon/Maths/Prediction.cs
+++ MoonDiana/ShineCommon/Maths/Prediction.cs
@@ -2,18 +2,18 @@
  Copyright 2015 - 2015 SPrediction
  Prediction.cs is part of SPrediction
  
- ShineCommon is free software: you can redistribute it and/or modify
+ SPrediction is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
  
- ShineCommon is distributed in the hope that it will be useful,
+ SPrediction is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  GNU General Public License for more details.
  
  You should have received a copy of the GNU General Public License
- along with ShineCommon. If not, see <http://www.gnu.org/licenses/>.
+ along with SPrediction. If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
